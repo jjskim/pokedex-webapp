ba8e0fdbf0e9e49b4432bd9d97420c1bd8b3240e
Adam Walls <awalls1@gmail.com>
2017-09-05 17:18:49 -0700

    back button added, carousel hiding now functional

--- public/scripts/views/aboutView.js
+++ public/scripts/views/aboutView.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// hide Back button on page load
+$( '#backButton' ).hide(10);
+
 $('header a' ).on('click', function(e) {
   console.log('Home clicked');
   e.preventDefault();
@@ -8,7 +11,9 @@ $('header a' ).on('click', function(e) {
   $( '#faceClick1' ).show('fast');
   $( '#faceClick2' ).show('fast');
   $( '#faceClick3' ).show('fast');
+  $( '#homeButton' ).show('fast');
 
+  $( '#backButton' ).hide('fast');
   $( '#face1Data' ).hide('fast');
   $( '#face2Data' ).hide('fast');
   $( '#face3Data' ).hide('fast');
@@ -22,7 +27,9 @@ $('#faceClick1' ).on('click', function(e) {
   $( '#faceClick2' ).hide('fast');
   $( '#faceClick3' ).hide('fast');
   $( '.carousel p' ).hide('fast');
+  $( '#homeButton' ).hide('fast');
 
+  $( '#backButton' ).show('fast');
   $( '#face1Data' ).show('fast');
 });
 
@@ -34,7 +41,9 @@ $('#faceClick2' ).on('click', function(e) {
   $( '#faceClick1' ).hide('fast');
   $( '#faceClick3' ).hide('fast');
   $( '.carousel p' ).hide('fast');
+  $( '#homeButton' ).hide('fast');
 
+  $( '#backButton' ).show('fast');
   $( '#face2Data' ).show('fast');
 });
 
@@ -46,6 +55,8 @@ $('#faceClick3' ).on('click', function(e) {
   $( '#faceClick2' ).hide('fast');
   $( '#faceClick1' ).hide('fast');
   $( '.carousel p' ).hide('fast');
+  $( '#homeButton' ).hide('fast');
 
+  $( '#backButton' ).show('fast');
   $( '#face3Data' ).show('fast');
 });
